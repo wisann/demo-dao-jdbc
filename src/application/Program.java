72ecfaa273a0df4c575cbf65c188cb48239545c3
Wisan <wisansilva3@gmail.com>
2023-05-30 17:45:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		
-		System.out.println(obj);
+		Seller sel = new Seller(2, "wisan", "wisan@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(sel);
 
 	}
 
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -52,7 +52,7 @@ public class Department implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Department id =" + id + ", name =" + name;
+		return "Department id =" + id + ", name = " + name;
 	}
 
 
