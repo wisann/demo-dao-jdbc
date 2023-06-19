c02b86c02e92edf601e8a9f0e9312fc5491da56b
Wisan <wisansilva3@gmail.com>
2023-06-19 19:20:03 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,16 @@ package application;
 
 import java.util.Date;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller sel = new Seller(2, "wisan", "wisan@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(sel);
-
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+		Seller seller = sellerdao.findById(3);
+		System.out.println(seller);
 	}
 
 }
